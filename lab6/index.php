d2a40ae37ee5e186bba71356191ce14399d7e05f
KinMar100 <92027623+KinMar100@users.noreply.github.com>
2023-11-16 14:53:41 +0100

    lab6

---
+++ lab6/index.php
@@ -0,0 +1,56 @@
+<?php
+# version 1.5
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+include("php/head.inc.php");
+require('php/navbar.inc.php');
+
+
+if($_GET['idp'] == '') {
+    $strona = 'html/glowna.html';
+}
+elseif($_GET['idp'] == 'zolw_1'){
+    $strona = 'html/zolw_blotny.html';
+}
+elseif($_GET['idp'] == 'zolw_2'){
+    $strona = 'html/zolw_czerwonolicy.html';
+}
+elseif($_GET['idp'] == 'zolw_3'){
+    $strona = 'html/zolw_zoltobrzuchy.html';
+}
+elseif($_GET['idp'] == 'zolw_4'){
+    $strona = 'html/zolw_zoltolicy.html';
+}
+elseif($_GET['idp'] == 'info'){
+    $strona = 'html/info.html';
+}
+elseif($_GET['idp'] == 'funkcje'){
+    $strona = 'html/funkcje.html';
+}
+elseif($_GET['idp'] == 'filmy'){
+    $strona = 'html/filmy.html';
+}
+else {
+    echo "Page does not exist!";
+    exit;
+}
+
+include($strona);
+
+foreach (glob("css/*.css") as $plikCss) {
+    echo '<link rel="stylesheet" href="' . $plikCss . '">';
+}
+foreach (glob("js/*.js") as $plikJs) {
+    echo '<script src="' . $plikJs . '"></script>';
+}
+
+$nr_indeksu = "164407";
+$nrGrupy = "2";
+$imieNazwisko = "Kinga Markowska";
+
+echo "<br/>PHP:<br/>";
+
+echo "Autor: ".$imieNazwisko." ".$nr_indeksu." grupa ".$nrGrupy." <br/><br/>";
+
+require('php/footer.inc.php');
---
+++ lab6/v.1.4/labor_164407_2.php
@@ -0,0 +1,85 @@
+<?php
+// ZADANIE_1
+$nr_indeksu = '164407';
+$nrGrupy = '2';
+
+echo 'Kinga Markowska '.$nr_indeksu.' grupa '.$nrGrupy.'<br/><br/>';
+
+// ZADANIE_2
+
+// podpunkt_a
+echo 'Zastosowanie metody include(): <br/>';
+include ('vars.php');
+echo "A $color $fruit <br/>";
+
+echo 'Zastosowanie metody require_once(): <br/>';
+$req_once = require_once("vars.php");
+echo "Tekst z require_once:".$req_once.'<br/>';
+
+// podpunkt_b
+
+$your_age = 21;
+$voting_age = 18;
+
+if ($your_age >= $voting_age) {
+    echo "Mozesz glosowac | Twoj wiek: ".$your_age."<br/>";
+}
+elseif ($your_age < $voting_age & $your_age > 0) {
+    echo "Nie mozesz glosowac | Twoj wiek: ".$your_age."<br/>";
+}
+else{
+    echo "Podano zła wartosc wieku | Wartosc wieku: ".$your_age."<br/>";
+}
+
+// podpunkt_c
+
+// petla_while
+$iterator = 0;
+$suma1 = 0;
+echo "Wartosci elementow: <br/>";
+while($iterator <= 10) {
+    echo $iterator." element: ";
+    if ($iterator == 10) {
+        echo $suma1."<br/>";
+        break;
+    }
+    echo $suma1."<br/>";
+    $iterator++;
+    $suma1 = $suma1 + $iterator;
+}
+echo "Suma elementow od 0 do 10: ".$suma1."<br/> <br/>";
+
+// petla_for
+$suma2 = 0;
+for ($iterator = 0; $iterator <= 10; $iterator++)
+{
+    echo $iterator." element: ";
+    if ($iterator == 10) {
+        $suma2 = $suma2 + $iterator;
+        echo $suma2."<br/>";
+        break;
+    }
+    echo $suma2."<br/>";
+
+    $suma2 = $suma2 + $iterator;
+}
+echo "Suma elementow od 0 do 10: ".$suma2."<br/> <br/>";
+
+
+
+// podpunkt_d
+
+//typ zmiennej $_GET
+
+echo 'Hej '.htmlspecialchars($_GET["nazwa"].'!');
+
+//typ zmiennej $_POST
+
+echo 'Hej '.htmlspecialchars($_POST["nazwa"]) . '!';
+
+//typ zmiennej $_SESSION
+
+session_start();
+$val = 0;
+$_SESSION["nowa_sesja"]=$val;
+echo $_SESSION["nowa_sesja"];
