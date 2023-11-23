fc5a7eea83edc0c64bb77c60dc59fb0568fcef53
KinMar100 <92027623+KinMar100@users.noreply.github.com>
2023-11-23 14:51:07 +0100

    lab7_toedit

---
+++ lab7/cfg.php
@@ -0,0 +1,18 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$baza = "moja_strona";
+
+$link = mysqli_connect($dbhost,$dbuser,$dbpass);
+if(!$link)
+{
+    echo die("<b>przerwane połączenie </b>");
+}
+if(!mysqli_select_db($link, $baza))
+{
+    echo "nie wybrano bazy";
+}
+
+$login = "kinga";
+$pass = "kinmar";
---
+++ lab7/index.php
@@ -0,0 +1,64 @@
+<?php
+# version 1.6
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+include("cfg.php");
+
+foreach (glob("css/*.css") as $plikCss) {
+    echo '<link rel="stylesheet" href="' . $plikCss . '">';
+}
+foreach (glob("js/*.js") as $plikJs) {
+    echo '<script src="' . $plikJs . '"></script>';
+}
+include("php/head.php");
+require('php/navbar.php');
+
+
+if($_GET['idp'] == '') {
+    $strona = 'html/glowna.html';
+}
+elseif($_GET['idp'] == 'zolw_1'){
+    $strona = 'html/zolw_blotny.html';
+}
+elseif($_GET['idp'] == 'zolw_2'){
+    $strona = 'html/zolw_czerwonolicy.html';
+}
+elseif($_GET['idp'] == 'zolw_3'){
+    $strona = 'html/zolw_zoltobrzuchy.html';
+}
+elseif($_GET['idp'] == 'zolw_4'){
+    $strona = 'html/zolw_zoltolicy.html';
+}
+elseif($_GET['idp'] == 'info'){
+    $strona = 'html/info.html';
+}
+elseif($_GET['idp'] == 'funkcje'){
+    $strona = 'html/funkcje.html';
+}
+elseif($_GET['idp'] == 'filmy'){
+    $strona = 'html/filmy.html';
+}
+else {
+    echo "Page does not exist!";
+    exit;
+}
+
+if (file_exists($strona))
+{
+    include($strona);
+}
+else
+{
+    echo "The file $strona does not exist";
+}
+
+$nr_indeksu = "164407";
+$nrGrupy = "2";
+$imieNazwisko = "Kinga Markowska";
+
+echo "<br/>PHP:<br/>";
+
+echo "Autor: ".$imieNazwisko." ".$nr_indeksu." grupa ".$nrGrupy." <br/><br/>";
+
+require('php/footer.php');
